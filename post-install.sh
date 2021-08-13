f11b1010fe582cc35a4b5fa36d62c0ec9eadc8f4
LostDev <82785402+intLostDev@users.noreply.github.com>
2021-08-13 18:19:43 -0400

    Add files via upload

--- post-install.sh
+++ post-install.sh
@@ -2,37 +2,35 @@
 
 cd
 
-sudo pacman-mirrors -f && sudo pacman -Syyu -y
+sudo pacman -S git 
 
-sudo pacman -S base-devel -y
+sudo pacman -S snapd 
 
-sudo pacman -S git -y 
+sudo systemctl enable --now snapd.socket 
 
-sudo pacman -S snapd -y
+sudo ln -s /var/lib/snapd/snap /snap\ 
 
-sudo systemctl enable --now snapd.socket -y
+sudo snap install blender --classic 
 
-sudo ln -s /var/lib/snapd/snap /snap\ -y
+sudo snap install discord 
 
-sudo snap install blender --classic -y
+sudo snap install minecraft-launcher-ot 
 
-sudo snap install discord -y
+sudo snap install chromium 
 
-sudo snap install minecraft-launcher-ot -y
+sudo snap install handbrake-jz 
 
-sudo snap install chromium -y
+sudo snap install darktable 
 
-sudo snap install handbrake-jz -y
+sudo snap install inkscape 
 
-sudo snap install darktable -y
+sudo snap install obs-studio 
 
-sudo snap install inkscape -y
+sudo snap install audacity
 
-sudo snap install obs-studio -y
+sudo snap install code --classic 
 
-sudo snap install code --classic -y
-
-sudo pacman -S lutris -y
+sudo pacman -S lutris 
 
 git clone https://github.com/AUNaseef/protonup && cd protonup
 python3 setup.py install --user
@@ -40,13 +38,13 @@ python3 setup.py install --user
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 
-sudo pacman -Syu -y
+sudo pacman -Syu 
 
-sudo pacman -S wine -y
+sudo pacman -S wine 
 
-sudo pacman -S --asdep lib32-gnutls lib32-openssl lib32-pipewire lib32-libpulse lib32-alsa-lib lib32-alsa-plugins -y
+sudo pacman -S --asdep lib32-gnutls lib32-openssl lib32-pipewire lib32-libpulse lib32-alsa-lib lib32-alsa-plugins 
 
-sudo pacman -S git python-pip cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info -y
+sudo pacman -S git python-pip cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info 
 
 git clone https://gitlab.com/brinkervii/grapejuice.git /tmp/grapejuice
 
@@ -54,15 +52,7 @@ cd /tmp/grapejuice
 ./install.py
 
 
-sudo pacman -S expac -y
+sudo pacman -S expac 
 
-sudo pacman -S $(expac '%n %o' | grep ^wine) -y
-
-cd
-
-git clone https://aur.archlinux.org/heroic-games-launcher-bin.git
-
-cd heroic-games-launcher-bin
-
-makepkg --cleanbuild --syncdeps --install --clean --rmdeps
+sudo pacman -S $(expac '%n %o' | grep ^wine) 
 
