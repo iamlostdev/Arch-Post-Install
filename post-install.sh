9129cf393688eedf7410304dffacb0d626995305
LostDev <82785402+intLostDev@users.noreply.github.com>
2021-11-15 23:15:08 -0500

    added neofetch

--- post-install.sh
+++ post-install.sh
@@ -48,6 +48,8 @@ sudo pacman -S code
 
 sudo pacman -S obs-studio
 
+sudo pacman -S neofetch
+
 sudo pacman -S --asdep lib32-gnutls lib32-openssl lib32-pipewire lib32-libpulse lib32-alsa-lib lib32-alsa-plugins 
 
 sudo pacman -S git python-pip cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info 
