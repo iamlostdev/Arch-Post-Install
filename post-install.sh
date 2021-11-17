cfb4c698be345a3768a52179e87e34f6e97169ff
LostDev <82785402+intLostDev@users.noreply.github.com>
2021-11-16 23:33:05 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -113,4 +113,4 @@ git clone https://github.com/intLostDev/Arch-Grapejuice-Mouse-Fix
 
 cd Arch-Grapejuice-Mouse-Fix
 
-sudo pacman -U wine-tkg-staging-fsync-git-6.11.r4.g0dd44a25-326-x86_64.pkg.tar.zst
+sudo pacman -U wine-tkg-fsync-git-6.14.r0.g7bedd313c88-326-x86_64.pkg.tar.zst
