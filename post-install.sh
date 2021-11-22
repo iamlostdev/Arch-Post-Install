5d239db987e9ec2c37c3e5726a115b5ac944cb80
LostDev <82785402+intLostDev@users.noreply.github.com>
2021-11-22 02:17:14 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -78,6 +78,8 @@ sudo pacman -S brave
 
 sudo pacman -S firefox
 
+sudo pacman -S dosbox
+
 sudo pacman -S --asdep lib32-gnutls lib32-openssl lib32-pipewire lib32-libpulse lib32-alsa-lib lib32-alsa-plugins 
 
 sudo pacman -S git python-pip cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info 
