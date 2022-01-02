9e71f0dd5ff66001ddd9ecee224bfe9e64a4b54a
LostDev <82785402+intLostDev@users.noreply.github.com>
2022-01-02 15:34:33 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -96,6 +96,8 @@ sudo pacman -S tor-browser
 
 sudo pacman -S kdenlive
 
+sudo pacman -S dxvk
+
 sudo pacman -S --asdep lib32-gnutls lib32-openssl lib32-pipewire lib32-libpulse lib32-alsa-lib lib32-alsa-plugins 
 
 sudo pacman -S git python-pip cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info 
