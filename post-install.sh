941c7e2f4de8557a649cc577d0b2fe1a55994849
LostDev <82785402+intLostDev@users.noreply.github.com>
2022-01-05 14:53:01 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com 
+
+sudo pacman-key --lsign-key FBA220DFC880C036
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sudo echo "[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" >> sudo /etc/pacman.conf
+
+
 echo script is designed for a desktop environment and install to be finished
 
 cd ~
