8fe43b0c949a2674b174d295f302cd3aae254f47
intLostDev <mavery735@gmail.com>
2022-10-16 15:16:40 -0400

    made vm's work by default

--- post-install.sh
+++ post-install.sh
@@ -16,7 +16,7 @@ sudo pacman -Syu
 
 sudo pacman -Syyu
 
-paru -S ulauncher meshroom alvr oversteer putty gamemode multimc protonup github-desktop grapejuice npm salad protonvpn kdenlive davinci-resolve pulseaudio-alsa k3b expac virt-manager qemu ovmf libvirt hyperv dnsmasq feh swtpm meson gparted python-pip cmake printer-support anaconda lutris kitty topgrade htop neovim helix vi vim-plug thunar vscodium handbrake redshift spotify spotify-adkiller pcsx2 wireshark minecraft-launcher qbittorrent heroic-games-launcher ferdi goxel bottles firefox rawtherapee celluloid audacity darktable inkscape gimp steam blender libreoffice-fresh discord filezilla brave dosbox tor protonup thunderbird wine gnome-disk-utility nautilus gvfs-mtp speedify speedifyui brew flatpak
+paru -S ulauncher meshroom alvr oversteer putty gamemode multimc protonup github-desktop grapejuice npm salad protonvpn kdenlive davinci-resolve pulseaudio-alsa k3b expac virt-manager qemu qemu-arch-extra ovmf libvirt hyperv dnsmasq feh swtpm meson gparted python-pip cmake printer-support anaconda lutris kitty topgrade htop neovim helix vi vim-plug thunar vscodium handbrake redshift spotify spotify-adkiller pcsx2 wireshark minecraft-launcher qbittorrent heroic-games-launcher ferdi goxel bottles firefox rawtherapee celluloid audacity darktable inkscape gimp steam blender libreoffice-fresh discord filezilla brave dosbox tor protonup thunderbird wine gnome-disk-utility nautilus gvfs-mtp speedify speedifyui brew flatpak
 
 brew install libdvdcss
 
@@ -27,6 +27,7 @@ paru -S --asdep lib32-gnutls lib32-openssl lib32-pipewire lib32-libpulse lib32-a
 conda install numba cudatoolkit
 
 cargo install remodel
+
 cd $HOME
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -70,6 +71,10 @@ wget https://github.com/dimtpap/obs-pipewire-audio-capture/releases/download/1.0
 tar -zxvf linux-pipewire-audio-1.0.5-obs-27-compat.tar.gz -C $HOME/.var/app/com.obsproject.Studio/config/obs-studio/plugins
 
 cd $HOME
+
+sudo usermod -a -G libvirt "$(whoami)"
+
+sudo usermod -a -G kvm "$(whoami)"
 
 sudo virsh net-autostart default
 
